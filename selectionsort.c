327de8784d49adc259efa13e05f555c0defbc491
witty-singh <87194791+witty-singh@users.noreply.github.com>
2021-10-16 13:10:37 +0530

    Update selectionsort.c

--- selectionsort.c
+++ selectionsort.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 int main(){
-    int n;
-    printf("Enter array length:-\n");
-    scanf("%d",&n);
-    int arr[n];
-    printf("Enter array:-\n");
-    for(int i=0;i<n;i++){
+    int n,i,min,temp;
+    int arr[100];
+    printf("Enter length of array:\n");
+    scanf("%d ",&n);
+    printf("Enter array elements:-\n");
+    for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    for(int i=0;i<n-1;i++){
+    for(i=0;i<n-1;i++){
+        min=i;
         for(int j=i+1;j<n;j++){
-            if(arr[i]>arr[j]){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-            }
+            if(arr[j]>arr[min])
+                min=j;
         }
+                temp=arr[min];
+                arr[min]=arr[i];
+                arr[i]=temp;
     }
     printf("After selection sort");
     for(int i=0;i<n;i++){
